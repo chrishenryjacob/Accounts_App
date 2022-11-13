a11c5e7e1d2848072b0bc63df3242181b2474556
Christopher <christopherhenryjacob@gmail.com>
2022-11-13 15:43:00 +0530

    initial app skeleton added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,18 @@ import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { NzPageHeaderModule } from 'ng-zorro-antd/page-header';
+import { NzCardModule } from 'ng-zorro-antd/card';
+import { NzInputModule } from 'ng-zorro-antd/input';
+import { NzGridModule } from 'ng-zorro-antd/grid';
+import { NzUploadModule } from 'ng-zorro-antd/upload';
+import { NzMessageModule } from 'ng-zorro-antd/message';
+import { NzIconModule } from 'ng-zorro-antd/icon';
+import { NzButtonModule } from 'ng-zorro-antd/button';
+import { NzDatePickerModule } from 'ng-zorro-antd/date-picker';
+import { NzDropDownModule } from 'ng-zorro-antd/dropdown';
+import { NzPopconfirmModule } from 'ng-zorro-antd/popconfirm';
+
 registerLocaleData(en);
 
 @NgModule({
@@ -20,7 +32,18 @@ registerLocaleData(en);
     BrowserModule,
     FormsModule,
     HttpClientModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    NzPageHeaderModule,
+    NzCardModule,
+    NzInputModule,
+    NzGridModule,
+    NzUploadModule,
+    NzMessageModule,
+    NzIconModule,
+    NzButtonModule,
+    NzDatePickerModule,
+    NzDropDownModule,
+    NzPopconfirmModule
   ],
   providers: [
     { provide: NZ_I18N, useValue: en_US }
